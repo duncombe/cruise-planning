fad17761f5a50c7483c966f763cfe42e8be8f7bd
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-04-22 13:07:18 +0200

    Updating config.zsh

--- config.zsh
+++ config.zsh
@@ -1,17 +1,16 @@
 #! /bin/zsh
 
-
-export TITLE="Campaign Name Survey Grid"
+export TITLE="ECMS High-Density Cruise Plan"
 
 export STATIONSFILE=allstns.stns
 
 export TOPO=/usr/local/bathy/etopo1/ETOPO1_Ice_g_gmt4-.grd
 
-# defines line spacing (n.miles)
-export LSPACENM=30.0
+# defines line spacing
+export LSPACENM=5.0
 
-# defines station spacing (n.miles)
-export SSPACENM=10.0
+# defines station spacing
+export SSPACENM=0.5
 
 # defines line orientation (compass direction)
 export ANGLE=125
@@ -19,33 +18,33 @@ export ANGLE=125
 # defines area over which to calculate stations.
 # Counts NUP lines north of origin
 # Counts NDOWN lines south of origin
-export NUP=0
-export NDOWN=6
+export NUP=-4
+export NDOWN=120
 
 # defines number of stations in a line
-export NSTN=12
+export NSTN=411
 
 # near Maputo to Port Elizabeth	
-export ORIGIN=27.91666670/-33.03333330 
+export ORIGIN=32.57703200/-25.96559000
 export REMOTE=25.60000000/-33.96666667
 
 # only print stations at the end of lines
 export LINELIMITS=true
 
 # only do stations over ground that is between MAXDEPTH And MINDEPTH
-export MAXDEPTH=9999
-export MINDEPTH=-9999
+export MAXDEPTH=351
+export MINDEPTH=20
 # Departure and arrival ports
-export DEPART="27.91666670	-33.03333330	 East London"
+export DEPART="31.00000000	-29.88333333	 Durban"
 export ARRIVE="25.60000000	-33.96666667	 Port Elizabeth"
 
 export TIMEONSTATION=0
-export SPEED=10
+export SPEED=8
 export MAXCAST=1500
 export SAMPLES=12
 # For GENERIC MAPPING TOOLS figures
 # Range also partly defines area over which to read coastline 
 # database and limits of line calculation 
-export Range=-R24/30.5/-36.0/-32.0
+export Range=-R20/35/-36.5/-20.5
 export Proj=-JM5i
 
