b1e19447eba0d89ff0fab56d918be67e4c268936
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-05-04 12:01:17 +0200

    Merged up to master and adjusted to use config.zsh

--- config.zsh
+++ config.zsh
@@ -1,56 +1,55 @@
 #! /bin/zsh
 
 
-export TITLE="Campaign Name Survey Grid"
+export TITLE="IEP Tow-Yo Cruise Plan"
 
 export STATIONSFILE=allstns.stns
 
 export TOPO=/usr/local/bathy/etopo1/ETOPO1_Ice_g_gmt4-.grd
 
 # defines line spacing (n.miles)
-export LSPACENM=30.0
+export LSPACENM=10.0
 
 # defines station spacing (n.miles)
-export SSPACENM=10.0
+export SSPACENM=1.0
 
 # defines line orientation (compass direction)
-export ANGLE=125
+export ANGLE=245
 
 # defines area over which to calculate stations.
 # Counts NUP lines north of origin
 # Counts NDOWN lines south of origin
-export NUP=0
-export NDOWN=6
+export NUP=-1
+export NDOWN=38
 
 # defines number of stations in a line
-export NSTN=12
+export NSTN=201
 
-# near Maputo to Port Elizabeth	
-export ORIGIN=27.91666670/-33.03333330 
-export REMOTE=25.60000000/-33.96666667
+export ORIGIN=16.45/-28.6333333    
+export REMOTE=16.433/-32.7442
 
 # only print stations at the end of lines
 export LINELIMITS=true
 
 # only do stations over ground that is between MAXDEPTH And MINDEPTH
-export MAXDEPTH=9999
-export MINDEPTH=-9999
+export MAXDEPTH=1501
+export MINDEPTH=50
 
 # ensure stations are more than SKIPNM and LINENM nautical mile apart
 export SKIPNM=0
 export LINENM=0
 
 # Departure and arrival ports
-export DEPART="27.91666670	-33.03333330	 East London"
-export ARRIVE="25.60000000	-33.96666667	 Port Elizabeth"
+export DEPART="18.43666667    -33.90833333     Cape Town Docks"
+export ARRIVE="18.43666667    -33.90833333     Cape Town Docks"
 
-export TIMEONSTATION=0
-export SPEED=10
-export MAXCAST=1500
-export SAMPLES=12
+export TIMEONSTATION=1
+export SPEED=8
+export MAXCAST=0
+export SAMPLES=0
 # For GENERIC MAPPING TOOLS figures
 # Range also partly defines area over which to read coastline 
 # database and limits of line calculation 
-export Range=-R24/30.5/-36.0/-32.0
+export Range=-R10/20/-36.5/-27.5 
 export Proj=-JM5i
 
